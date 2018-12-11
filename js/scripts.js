9c1da43ba9d49fc77282f043b1bc44046f617568
TS Planning Engineer <ym5@ualberta.ca>
2018-12-11 10:18:46 -0700

    change snow

--- js/scripts.js
+++ js/scripts.js
@@ -89,6 +89,118 @@
     function styleDefault(){
         $('#particles-js').css("background-color", "#3B4254");
         //https://github.com/VincentGarreau/particles.js/
+        //
+        // particlesJS("particles-js", {
+        //     "particles": {
+        //         "number": {
+        //             "value": 400,
+        //             "density": {
+        //                 "enable": true,
+        //                 "value_area": 800
+        //             }
+        //         },
+        //         "color": {
+        //             "value": "#ffffff"
+        //         },
+        //         "shape": {
+        //             "type": "image",
+        //             "stroke": {
+        //                 "width": 3,
+        //                 "color": "#fff"
+        //             },
+        //             "polygon": {
+        //                 "nb_sides": 5
+        //             },
+        //             "image": {
+        //                 "src": "./images/snow.png",
+        //                 "width": 60,
+        //                 "height": 60
+        //             }
+        //         },
+        //         "opacity": {
+        //             "value": 0.7,
+        //             "random": false,
+        //             "anim": {
+        //                 "enable": false,
+        //                 "speed": 1,
+        //                 "opacity_min": 0.1,
+        //                 "sync": false
+        //             }
+        //         },
+        //         "size": {
+        //             "value": 3,
+        //             "random": true,
+        //             "anim": {
+        //                 "enable": false,
+        //                 "speed": 20,
+        //                 "size_min": 0.5,
+        //                 "sync": false
+        //             }
+        //         },
+        //         "line_linked": {
+        //             "enable": false,
+        //             "distance": 50,
+        //             "color": "#ffffff",
+        //             "opacity": 0.6,
+        //             "width": 1
+        //         },
+        //         "move": {
+        //             "enable": true,
+        //             "speed": 4,
+        //             "direction": "bottom",
+        //             "random": true,
+        //             "straight": false,
+        //             "out_mode": "out",
+        //             "bounce": false,
+        //             "attract": {
+        //                 "enable": true,
+        //                 "rotateX": 300,
+        //                 "rotateY": 1200
+        //             }
+        //         }
+        //     },
+        //     "interactivity": {
+        //         "detect_on": "canvas",
+        //         "events": {
+        //             "onhover": {
+        //                 "enable": true,
+        //                 "mode":  "bubble"
+        //             },
+        //             "onclick": {
+        //                 "enable": true,
+        //                 "mode": "repulse"
+        //             },
+        //             "resize": true
+        //         },
+        //         "modes": {
+        //             "grab": {
+        //                 "distance": 150,
+        //                 "line_linked": {
+        //                     "opacity": 1
+        //                 }
+        //             },
+        //             "bubble": {
+        //                 "distance": 200,
+        //                 "size": 40,
+        //                 "duration": 2,
+        //                 "opacity": 8,
+        //                 "speed": 3
+        //             },
+        //             "repulse": {
+        //                 "distance": 200,
+        //                 "duration": 0.2
+        //             },
+        //             "push": {
+        //                 "particles_nb": 4
+        //             },
+        //             "remove": {
+        //                 "particles_nb": 2
+        //             }
+        //         }
+        //     },
+        //     "retina_detect": true
+        // });
+        //https://github.com/VincentGarreau/particles.js/
 
         particlesJS("particles-js", {
             "particles": {
@@ -112,9 +224,9 @@
                         "nb_sides": 5
                     },
                     "image": {
-                        "src": "./images/snow.png",
-                        "width": 60,
-                        "height": 60
+                        "src": "http://www.dynamicdigital.us/wp-content/uploads/2013/02/starburst_white_300_drop_2.png",
+                        "width": 100,
+                        "height": 100
                     }
                 },
                 "opacity": {
@@ -128,12 +240,12 @@
                     }
                 },
                 "size": {
-                    "value": 3,
+                    "value": 5,
                     "random": true,
                     "anim": {
                         "enable": false,
                         "speed": 20,
-                        "size_min": 0.5,
+                        "size_min": 0.1,
                         "sync": false
                     }
                 },
@@ -146,7 +258,7 @@
                 },
                 "move": {
                     "enable": true,
-                    "speed": 4,
+                    "speed": 5,
                     "direction": "bottom",
                     "random": true,
                     "straight": false,
@@ -200,116 +312,6 @@
             },
             "retina_detect": true
         });
-        // particlesJS("particles-js", {
-        //     "particles": {
-        //         "number": {
-        //             "value": 80,
-        //             "density": {
-        //                 "enable": true,
-        //                 "value_area": 800
-        //             }
-        //         },
-        //         "color": {
-        //             "value": "#ffffff"
-        //         },
-        //         "shape": {
-        //             "type": "circle",
-        //             "stroke": {
-        //                 "width": 0,
-        //                 "color": "#000000"
-        //             },
-        //             "polygon": {
-        //                 "nb_sides": 5
-        //             },
-        //             "image": {
-        //                 "src": "img/github.svg",
-        //                 "width": 100,
-        //                 "height": 100
-        //             }
-        //         },
-        //         "opacity": {
-        //             "value": 0.5,
-        //             "random": false,
-        //             "anim": {
-        //                 "enable": false,
-        //                 "speed": 1,
-        //                 "opacity_min": 0.1,
-        //                 "sync": false
-        //             }
-        //         },
-        //         "size": {
-        //             "value": 3,
-        //             "random": true,
-        //             "anim": {
-        //                 "enable": false,
-        //                 "speed": 40,
-        //                 "size_min": 0.1,
-        //                 "sync": false
-        //             }
-        //         },
-        //         "line_linked": {
-        //             "enable": true,
-        //             "distance": 150,
-        //             "color": "#ffffff",
-        //             "opacity": 0.4,
-        //             "width": 1
-        //         },
-        //         "move": {
-        //             "enable": true,
-        //             "speed": 6,
-        //             "direction": "none",
-        //             "random": false,
-        //             "straight": false,
-        //             "out_mode": "out",
-        //             "bounce": false,
-        //             "attract": {
-        //                 "enable": false,
-        //                 "rotateX": 600,
-        //                 "rotateY": 1200
-        //             }
-        //         }
-        //     },
-        //     "interactivity": {
-        //         "detect_on": "canvas",
-        //         "events": {
-        //             "onhover": {
-        //                 "enable": true,
-        //                 "mode": "grab"
-        //             },
-        //             "onclick": {
-        //                 "enable": true,
-        //                 "mode": "push"
-        //             },
-        //             "resize": true
-        //         },
-        //         "modes": {
-        //             "grab": {
-        //                 "distance": 400,
-        //                 "line_linked": {
-        //                     "opacity": 1
-        //                 }
-        //             },
-        //             "bubble": {
-        //                 "distance": 400,
-        //                 "size": 40,
-        //                 "duration": 2,
-        //                 "opacity": 8,
-        //                 "speed": 3
-        //             },
-        //             "repulse": {
-        //                 "distance": 200,
-        //                 "duration": 0.4
-        //             },
-        //             "push": {
-        //                 "particles_nb": 4
-        //             },
-        //             "remove": {
-        //                 "particles_nb": 2
-        //             }
-        //         }
-        //     },
-        //     "retina_detect": true
-        // });
     };
 
 
